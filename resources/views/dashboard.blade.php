a9597f9d4a4276e1b5b78fcfea3bfee3089e3cdf
Gacik Sasa <gacik.mail@gmail.com>
2025-03-08 23:37:00 +0100

    Update takmicenje to display even if NULL

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -62,7 +62,11 @@
                             </div>
                             <small>{{ $utakmica->datum->format('d.m.Y') }}</small>
                         </div>
-                        <small class="text-muted">{{ $utakmica->takmicenje->naziv }}</small>
+                        <small class="text-muted">
+                            @if($utakmica->takmicenje)
+                                {{ $utakmica->takmicenje->naziv }}
+                            @endif
+                        </small>
                     </a>
                     @endforeach
                 </div>
