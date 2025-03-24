ce31f3689707702ff0374f64821721c579aeea06
Gacik Sasa <gacik.mail@gmail.com>
2025-03-24 19:40:27 +0100

    Adding players sorting functionality to utakmice show

--- database/migrations/2025_03_24_192628_add_redosled_to_protivnicki_igraci.php
+++ database/migrations/2025_03_24_192628_add_redosled_to_protivnicki_igraci.php
@@ -4,19 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRedosledToProtivnickiIgraci extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::table('protivnicki_igraci', function (Blueprint $table) {
-            $table->integer('redosled')->nullable()->default(0);
+            $table->integer('redosled')->nullable()->after('u_sastavu');
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::table('protivnicki_igraci', function (Blueprint $table) {
             $table->dropColumn('redosled');
         });
     }
-}
+};
