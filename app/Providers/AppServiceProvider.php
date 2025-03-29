af83fb480c8af853d7ca15b4726d8716ca5d5966
Gacik Sasa <gacik.mail@gmail.com>
2025-03-29 15:35:51 +0100

    fixing bootstrap pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        
+        // Use Bootstrap for pagination
+        Paginator::useBootstrap();
         
         // Forsiraj HTTPS u produkciji
         if (config('app.env') === 'production') {
