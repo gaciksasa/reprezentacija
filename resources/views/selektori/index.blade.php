a33433ba703dc43bfa09bd981f59fcbfb00d5001
Gacik Sasa <gacik.mail@gmail.com>
2025-04-21 22:03:07 +0200

    Fixing selektori index sort by date

--- resources/views/selektori/index.blade.php
+++ resources/views/selektori/index.blade.php
@@ -34,151 +34,166 @@
                     </thead>
                     <tbody>
                         @php
-                            // Grupišemo selektore po mandatima koji su deo komisije
-                            $komisije = [];
-                            $samostalniSelektori = [];
+                            // Prvo prikupljamo sve mandate i pravilno ih sortiramo po početku mandata
+                            $sviMandati = collect();
                             
+                            // Dodajemo mandate komisija
                             foreach($selektori as $selektor) {
-                                // Proveriti da li je u komisiji koja već postoji u nizu
-                                $komisijskiMandat = $selektor->mandati->where('komisija', true)->first();
+                                $komisijaMandate = $selektor->mandati->where('komisija', true);
                                 
-                                if ($komisijskiMandat) {
-                                    $kljuc = $komisijskiMandat->tim_id . '-' . $komisijskiMandat->pocetak_mandata->format('Y-m-d');
+                                foreach($komisijaMandate as $mandat) {
+                                    // Dodaj samo jednom za jednu komisiju (po početku mandata i timu)
+                                    $kljuc = $mandat->tim_id . '-' . $mandat->pocetak_mandata->format('Y-m-d');
                                     
                                     if (!isset($komisije[$kljuc])) {
                                         $komisije[$kljuc] = [
-                                            'tim_id' => $komisijskiMandat->tim_id,
-                                            'tim_naziv' => $komisijskiMandat->tim->naziv,
-                                            'pocetak' => $komisijskiMandat->pocetak_mandata,
-                                            'kraj' => $komisijskiMandat->kraj_mandata,
-                                            'glavni_selektor' => null,
-                                            'clanovi' => [],
-                                            'statistika' => $komisijskiMandat->statistika,
-                                            'mandat_id' => $komisijskiMandat->id
+                                            'tim_id' => $mandat->tim_id,
+                                            'tim_naziv' => $mandat->tim->naziv,
+                                            'pocetak' => $mandat->pocetak_mandata,
+                                            'kraj' => $mandat->kraj_mandata,
+                                            'glavni_selektor' => $mandat->glavni_selektor ? $selektor : null,
+                                            'clanovi' => $mandat->glavni_selektor ? [] : [$selektor],
+                                            'statistika' => $mandat->statistika,
+                                            'mandat_id' => $mandat->id,
+                                            'tip' => 'komisija'
                                         ];
-                                    }
-                                    
-                                    if ($komisijskiMandat->glavni_selektor) {
-                                        $komisije[$kljuc]['glavni_selektor'] = $selektor;
+                                        
+                                        // Dodaj komisiju u sve mandate za sortiranje
+                                        $sviMandati->push((object)[
+                                            'id' => $kljuc,
+                                            'pocetak_mandata' => $mandat->pocetak_mandata,
+                                            'tip' => 'komisija'
+                                        ]);
                                     } else {
-                                        $komisije[$kljuc]['clanovi'][] = $selektor;
+                                        if ($mandat->glavni_selektor) {
+                                            $komisije[$kljuc]['glavni_selektor'] = $selektor;
+                                        } else {
+                                            $komisije[$kljuc]['clanovi'][] = $selektor;
+                                        }
                                     }
-                                } else {
-                                    $samostalniSelektori[] = $selektor;
                                 }
                             }
+                            
+                            // Dodajemo pojedinačne mandate
+                            foreach($selektori as $selektor) {
+                                $pojedinacniMandati = $selektor->mandati->where('komisija', false);
+                                
+                                foreach($pojedinacniMandati as $mandat) {
+                                    $mandat->selektor = $selektor;
+                                    $sviMandati->push($mandat);
+                                }
+                            }
+                            
+                            // Sortiramo sve mandate po datumu početka, od najnovijih ka najstarijim
+                            $sviMandati = $sviMandati->sortByDesc(function($mandat) {
+                                return $mandat->pocetak_mandata->timestamp;
+                            });
                         @endphp
                         
-                        {{-- Prvo prikazujemo komisije --}}
-                        @foreach($komisije as $komisija)
-                        <tr>
-                            <td>
-                                <strong>Selektorska komisija:</strong><br>
-                                @if($komisija['glavni_selektor'])
-                                    <a href="{{ route('selektori.show', $komisija['glavni_selektor']) }}">
-                                        {{ $komisija['glavni_selektor']->prezime }} {{ $komisija['glavni_selektor']->ime }}
-                                    </a>
-                                    <span class="badge bg-primary">Glavni</span><br>
-                                @endif
-                                
-                                @foreach($komisija['clanovi'] as $clan)
-                                    <a href="{{ route('selektori.show', $clan) }}">
-                                        {{ $clan->prezime }} {{ $clan->ime }}
-                                    </a><br>
-                                @endforeach
-                            </td>
-                            <td>
-                                {{ $komisija['pocetak']->format('d.m.Y') }} - 
-                                {{ $komisija['kraj'] ? $komisija['kraj']->format('d.m.Y') : 'danas' }}
-                            </td>
-                            <td>
-                                @if($komisija['statistika']['utakmice'] > 0)
-                                    {{ $komisija['statistika']['utakmice'] }} utakmice<br>
-                                    {{ $komisija['statistika']['pobede'] }}-{{ $komisija['statistika']['remiji'] }}-{{ $komisija['statistika']['porazi'] }}
-                                    ({{ $komisija['statistika']['procenatPobeda'] }}%)
-                                @else
-                                    -
-                                @endif
-                            </td>
-                            @if(Auth::check() && Auth::user()->hasEditAccess())
-                            <td>
-                                <div class="btn-group">
-                                    <a href="{{ route('selektor-komisija.edit', $komisija['mandat_id']) }}" class="btn btn-sm btn-warning">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                </div>
-                            </td>
-                            @endif
-                        </tr>
-                        @endforeach
-                        
-                        {{-- Zatim prikazujemo samostalne selektore --}}
-                        @foreach($samostalniSelektori as $selektor)
-                        <tr>
-                            <td>
-                                <a href="{{ route('selektori.show', $selektor) }}">
-                                    {{ $selektor->prezime }} {{ $selektor->ime }}
-                                </a>
-                                @if($selektor->aktivan)
-                                    <span class="badge bg-success">Aktivan</span>
-                                @endif
-                            </td>
-                            <td>
+                        @foreach($sviMandati as $mandat)
+                            @if(isset($mandat->tip) && $mandat->tip === 'komisija')
                                 @php
-                                    $prviMandat = $selektor->prviMandat;
-                                    $poslednjiMandat = $selektor->poslednjiMandat;
+                                    $komisija = $komisije[$mandat->id];
                                 @endphp
-                                
-                                @if($prviMandat && $poslednjiMandat)
-                                    {{ $prviMandat->pocetak_mandata->format('d.m.Y') }} - 
-                                    {{ $poslednjiMandat->kraj_mandata ? $poslednjiMandat->kraj_mandata->format('d.m.Y') : 'danas' }}
-                                    
-                                    @if($selektor->mandati->where('komisija', false)->count() > 1)
-                                        <span class="badge bg-info">{{ $selektor->mandati->where('komisija', false)->count() }} mandata</span>
+                                <tr>
+                                    <td>
+                                        @if($komisija['glavni_selektor'])
+                                            <a href="{{ route('selektori.show', $komisija['glavni_selektor']) }}">
+                                                {{ $komisija['glavni_selektor']->prezime }} {{ $komisija['glavni_selektor']->ime }}
+                                            </a><br>
+                                        @endif
+                                        
+                                        @foreach($komisija['clanovi'] as $clan)
+                                            <a href="{{ route('selektori.show', $clan) }}">
+                                                {{ $clan->prezime }} {{ $clan->ime }}
+                                            </a><br>
+                                        @endforeach
+                                    </td>
+                                    <td>
+                                        {{ $komisija['pocetak']->format('d.m.Y') }} - 
+                                        {{ $komisija['kraj'] ? $komisija['kraj']->format('d.m.Y') : 'danas' }}
+                                    </td>
+                                    <td>
+                                        @if($komisija['statistika']['utakmice'] > 0)
+                                            {{ $komisija['statistika']['utakmice'] }} utakmice<br>
+                                            {{ $komisija['statistika']['pobede'] }}-{{ $komisija['statistika']['remiji'] }}-{{ $komisija['statistika']['porazi'] }}
+                                            ({{ $komisija['statistika']['procenatPobeda'] }}%)
+                                        @else
+                                            -
+                                        @endif
+                                    </td>
+                                    @if(Auth::check() && Auth::user()->hasEditAccess())
+                                    <td>
+                                        <div class="btn-group">
+                                            <a href="{{ route('selektor-komisija.edit', $komisija['mandat_id']) }}" class="btn btn-sm btn-warning">
+                                                <i class="fas fa-edit"></i>
+                                            </a>
+                                        </div>
+                                    </td>
                                     @endif
-                                @else
-                                    -
-                                @endif
-                            </td>
-                            <td>
-                                @php
-                                    $statistika = $selektor->statistika;
-                                @endphp
-                                
-                                @if($statistika['utakmice'] > 0)
-                                    {{ $statistika['utakmice'] }} utakmice<br>
-                                    {{ $statistika['pobede'] }}-{{ $statistika['remiji'] }}-{{ $statistika['porazi'] }}
-                                    ({{ $statistika['procenatPobeda'] }}%)
-                                @else
-                                    -
-                                @endif
-                            </td>
-                            @if(Auth::check() && Auth::user()->hasEditAccess())
-                            <td>
-                                <div class="btn-group">
-                                    <a href="{{ route('selektori.show', $selektor) }}" class="btn btn-sm btn-info">
-                                        <i class="fas fa-eye"></i>
-                                    </a>
-                                    <a href="{{ route('selektori.edit', $selektor) }}" class="btn btn-sm btn-warning">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                    <button type="button" class="btn btn-sm btn-danger" 
-                                            onclick="if(confirm('Da li ste sigurni?')) document.getElementById('delete-selektor-{{ $selektor->id }}').submit()">
-                                        <i class="fas fa-trash"></i>
-                                    </button>
-                                    <form id="delete-selektor-{{ $selektor->id }}" action="{{ route('selektori.destroy', $selektor) }}" method="POST" class="d-none">
-                                        @csrf
-                                        @method('DELETE')
-                                    </form>
-                                </div>
-                            </td>
+                                </tr>
+                            @else
+                                <tr>
+                                    <td>
+                                        <a href="{{ route('selektori.show', $mandat->selektor) }}">
+                                            {{ $mandat->selektor->prezime }} {{ $mandat->selektor->ime }}
+                                        </a>
+                                        @if($mandat->kraj_mandata === null)
+                                            <span class="badge bg-success">Aktivan</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        {{ $mandat->pocetak_mandata->format('d.m.Y') }} - 
+                                        {{ $mandat->kraj_mandata ? $mandat->kraj_mandata->format('d.m.Y') : 'danas' }}
+                                        
+                                        @if($mandat->v_d_status)
+                                            <span class="badge bg-warning text-dark">v.d.</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @php
+                                            $mandatStatistika = $mandat->statistika;
+                                        @endphp
+                                        
+                                        @if($mandatStatistika['utakmice'] > 0)
+                                            {{ $mandatStatistika['utakmice'] }} utakmice<br>
+                                            {{ $mandatStatistika['pobede'] }}-{{ $mandatStatistika['remiji'] }}-{{ $mandatStatistika['porazi'] }}
+                                            ({{ $mandatStatistika['procenatPobeda'] }}%)
+                                        @else
+                                            -
+                                        @endif
+                                    </td>
+                                    @if(Auth::check() && Auth::user()->hasEditAccess())
+                                    <td>
+                                        <div class="btn-group">
+                                            <a href="{{ route('selektori.show', $mandat->selektor) }}" class="btn btn-sm btn-info">
+                                                <i class="fas fa-eye"></i>
+                                            </a>
+                                            <a href="{{ route('selektori.edit', $mandat->selektor) }}" class="btn btn-sm btn-warning">
+                                                <i class="fas fa-edit"></i>
+                                            </a>
+                                            <button type="button" class="btn btn-sm btn-danger" 
+                                                    onclick="if(confirm('Da li ste sigurni?')) document.getElementById('delete-mandat-{{ $mandat->id }}').submit()">
+                                                <i class="fas fa-trash"></i>
+                                            </button>
+                                            <form id="delete-mandat-{{ $mandat->id }}" 
+                                                    action="{{ route('selektori.obrisiMandat', $mandat->id) }}" 
+                                                    method="POST" class="d-none">
+                                                @csrf
+                                                @method('DELETE')
+                                            </form>
+                                        </div>
+                                    </td>
+                                    @endif
+                                </tr>
                             @endif
-                        </tr>
                         @endforeach
 
-                        @if(count($komisije) == 0 && count($samostalniSelektori) == 0)
+                        @if(empty($komisije) && $sviMandati->count() == 0)
                         <tr>
-                            <td colspan="5" class="text-center">Nema selektora u bazi podataka</td>
+                            <td colspan="{{ Auth::check() && Auth::user()->hasEditAccess() ? '4' : '3' }}" class="text-center">
+                                Nema selektora u bazi podataka
+                            </td>
                         </tr>
                         @endif
                     </tbody>
